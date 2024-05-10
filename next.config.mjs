9c91a0543c184ce9466b13bdcef60dcf877e5462
Zac Rosenbauer <zacrosenbauer@gmail.com>
2024-05-10 15:45:46 -0400

    💽 incremental change

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,6 @@ const nextConfig = {
   output: "export",
 
   /**
-   * Set base path. This is usually the slug of your repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/zrosenbauer.com",
-
-  /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
    *
@@ -25,7 +18,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-
 
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   experimental: {
