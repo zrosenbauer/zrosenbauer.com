78622ad985f7e275d73f3787678e060560082b00
Zac Rosenbauer <zacrosenbauer@gmail.com>
2024-05-10 15:07:06 -0400

    💽 incremental change

--- src/_future/blog/[slug]/header.tsx
+++ src/_future/blog/[slug]/header.tsx
@@ -75,7 +75,7 @@ export const Header: React.FC<{
         </div>
       </div>
       <div className='container mx-auto relative isolate overflow-hidden  py-24 sm:py-32'>
-        <div className='mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center'>
+        {/* <div className='mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center'>
           <div className='mx-auto max-w-2xl lg:mx-0'>
             <h1 className='text-4xl font-bold tracking-tight text-white sm:text-6xl font-display'>
               {project.title}
@@ -98,7 +98,7 @@ export const Header: React.FC<{
               ))}
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </header>
   );
